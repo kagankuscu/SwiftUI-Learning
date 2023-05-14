5329be4623dfc2522d1464e1b5fea5dc0851cb80
kagankuscu <kuscukagan@gmail.com>
2023-05-14 21:26:17 +0300

    Workıng with UI Controls - Section 1

---
+++ Landmarks/Views/Profiles/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Kagan Kuscu on 14.05.23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotification = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "k_kagan")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
