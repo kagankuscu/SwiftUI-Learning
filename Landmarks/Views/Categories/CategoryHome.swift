a7f1919391a3d4f94d4b9198a6a7bc45c713151b
kagankuscu <kuscukagan@gmail.com>
2023-05-18 14:03:32 +0300

    Interfacing with UIKit - Section 4

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -15,6 +15,9 @@ struct CategoryHome: View {
     var body: some View {
         NavigationView {
             List {
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0)})
+                    .aspectRatio(3 / 2, contentMode: .fit)
+                    .listRowInsets(EdgeInsets())
                 ForEach(modelData.categories.keys.sorted(), id: \.self ) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
