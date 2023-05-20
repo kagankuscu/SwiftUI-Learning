2686b7233506f74ce00635cf8c89aba6fce941a4
kagankuscu <kuscukagan@gmail.com>
2023-05-21 01:43:18 +0300

    MacOS - Section 3

--- Landmarks/Views/Landmark/LandmarkRow.swift
+++ Landmarks/Views/Landmark/LandmarkRow.swift
@@ -1,33 +1,43 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A single row to be displayed in a list of landmarks.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A single row to be displayed in a list of landmarks.
+ */
 
 import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
-
+    
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                .overlay {
-                    Circle().stroke(.white, lineWidth: 3)
-                }
-                .shadow(radius: 3.5)
-            Text(landmark.name)
-
+                .cornerRadius(5)
+            //                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            //                .overlay {
+            //                    Circle().stroke(.white, lineWidth: 3)
+            //                }
+            //                .shadow(radius: 3.5)
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .bold()
+                #if !os(watchOS)
+                Text(landmark.park)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                #endif
+            }
+            
             Spacer()
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
                     .foregroundColor(.yellow)
             }
         }
+        .padding(.vertical, 4)
     }
 }
 
