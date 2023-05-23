437f97683c48232fe05ef1835c93574b80cf0156
kagankuscu <kuscukagan@gmail.com>
2023-05-23 12:51:02 +0300

    Exploring the structure of a SwiftUI app

---
+++ LearningSwiftUI/LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Kagan Kuscu on 23.05.23.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
